7fc9a8c3d63c87c25a2933f0cfe0fd511e6cf300
A-Mitev <amitev03@gmail.com>
2015-11-26 16:57:28 +0200

    Up

--- 08_Sorting_Algoritms/src/Selection_Sort.java
+++ 08_Sorting_Algoritms/src/Selection_Sort.java
@@ -1,57 +1,24 @@
 import java.util.Arrays;
 
 public class Selection_Sort {
-	public static void main(String[] args) {
+	static void selectionSort(int[] num) {
+		for (int sortedCount = 1; sortedCount < num.length; sortedCount++) {
+			int maxIndex = 0;
+			for (int index = 0; index <= num.length - sortedCount; index++) {
+				if (num[index] > num[maxIndex]) {
+					maxIndex = index;
+				}
+			}
+			int temp = num[maxIndex];
+			num[maxIndex] = num[num.length - sortedCount];
+			num[num.length - sortedCount] = temp;
+		}
+	}
 
-////		int[] array = { 2, 1, 4, 3, 3, 8, 1, 4, 3 };
-////		int[] array = {1 , 3, 1, 1, 1, 1, 1, 4, 1 };
-//		int[] array = {8,5,4,7,1,1,5,7,8,7,8 };
-//
-//		int endIndex = array.length - 1;
-//		int startIndex = 0;
-//
-//		int currMaxIndex = 0;
-//		int currMinIndex = 0;
-//		int maxNumber = Integer.MIN_VALUE;
-//		int minNumber = Integer.MAX_VALUE;
-//
-//		// int loopItteration = array.length / 2 -1;
-//		// if(array[])
-//
-//		for (int i = 0; i < (array.length / 2) + 1; i++) {
-//			for (int index = i; index <= endIndex; index++) {
-//				if (array[index] > maxNumber) {
-//					maxNumber = array[index];
-//					currMaxIndex = index;
-//				}
-//				if (array[index] < minNumber) {
-//					minNumber = array[index];
-//					currMinIndex = index;
-//				}
-//			}
-//			int endIndexNumber = array[endIndex];
-//			array[endIndex] = array[currMaxIndex];
-//			array[currMaxIndex] = endIndexNumber;
-//			endIndex--;
-//			System.out.println(Arrays.toString(array));
-//
-////			if (currMinIndex == currMaxIndex){
-////				array[startIndex] = array[currMaxIndex];
-////				startIndex++;
-////				continue;
-////			}
-//			if (array[startIndex] == array[currMinIndex]){
-//				continue;
-//			}
-//			int startIndexNumber = array[startIndex];
-//			array[startIndex] = array[currMinIndex];
-//			array[currMinIndex] = startIndexNumber;
-//			startIndex++;
-//			System.out.println(Arrays.toString(array));
-//			maxNumber = Integer.MIN_VALUE;
-//			minNumber = Integer.MAX_VALUE;
-//		}
-//		System.out.println(Arrays.toString(array));
-//	}
-//}
-		
+	public static void main(String[] args) {
+		int[] num = { 40, 6, 82, 12, 41, 41, 59, -32, 61 };
+		selectionSort(num);
+		System.out.println(Arrays.toString(num));
+
+	}
+}
