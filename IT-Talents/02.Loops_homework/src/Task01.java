5220c755eb7d16038e401dd3ef604a9c33d3b605
A-Mitev <amitev03@gmail.com>
2015-12-04 00:44:23 +0200

    Upp

--- IT-Talents/02.Loops_homework/src/Task01.java
+++
@@ -1,8 +0,0 @@
-public class Task01 {
-	public static void main(String[] args) {
-		for (int index = 1; index < 101; index++) {
-			System.out.println(index);
-		}
-	}
-
-}
--- IT-Talents/02.Loops_homework/src/Task03.java
+++
@@ -1,8 +0,0 @@
-public class Task03 {
-	public static void main(String[] args) {
-		for (int index = -10; index <= 10 ;index+= 2) {
-			System.out.println(index);
-		}
-	}
-
-}
