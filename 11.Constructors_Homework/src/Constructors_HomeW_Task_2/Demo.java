dae09f3d755f413dd8112aa8ae4bc8fbf66e0f27
Nexus <amitev03@gmail.com>
2015-12-17 11:05:34 +0200

    Up Homework

---
+++ 11.Constructors_Homework/src/Constructors_HomeW_Task_2/Demo.java
@@ -0,0 +1,9 @@
+package Constructors_HomeW_Task_2;
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		StudentGroup java = new StudentGroup();
+		java.printStudentsInGroup();
+	}
+}
