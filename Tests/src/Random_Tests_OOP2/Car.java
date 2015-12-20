7de5d7cf371d213304f5f0c032a97cebe8feb00e
Nexus <amitev03@gmail.com>
2015-12-20 10:17:08 +0200

    up

---
+++ Tests/src/Random_Tests_OOP2/Car.java
@@ -0,0 +1,12 @@
+package Random_Tests_OOP2;
+public class Car {
+	String name;
+	int speed;
+	int gear;
+	int price;
+	
+	Car(String name){
+		this.name = name;
+	}
+
+}
---
+++ Tests/src/Random_Tests_OOP2/Demo.java
@@ -0,0 +1,11 @@
+package Random_Tests_OOP2;
+public class Demo {
+	public static void main(String[] args) {
+		
+	Car audi =  new Car("Audi");
+	System.out.println(audi.gear);
+	System.out.println(audi.name);
+	System.out.println(audi.price);
+	System.out.println(audi.speed);
+	}
+}
