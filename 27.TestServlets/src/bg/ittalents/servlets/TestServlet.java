445dbc263295a57af9d14fc1305043ce201435d7
A-Mitev <amitev03@gmail.com>
2016-03-18 09:13:40 +0200

    update to latest version 03.17

---
+++ 27.TestServlets/src/bg/ittalents/servlets/TestServlet.java
@@ -0,0 +1,27 @@
+package bg.ittalents.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Home")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public TestServlet() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter pw = response.getWriter();
+		pw.println("<h1>Veche znam kak se pravi servlet</h1>");
+		
+		
+	}
+
+}
